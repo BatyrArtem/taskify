9483cf6316aec69ef0a8373e9f2baabac99b55b3
BatyrArtem <artembatyr88@gmail.com>
2022-01-28 12:06:21 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+let name: string;
+let age: number | string;  
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+        <span className='heading'>Taskify</span>
+    </div>
+  );
+}
+
+export default App;
